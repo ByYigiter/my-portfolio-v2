c4967cd399b13cfcdb0851e34e0b95798191db83
Yigit Er <mr.yigiter222@gmail.com>
2023-10-22 18:31:02 +0300

    v1

--- src/Pages/Home/Pagination.js
+++ src/Pages/Home/Pagination.js
@@ -1,38 +1,35 @@
 import { useEffect, useState } from "react";
 import ReactPaginate from "react-paginate";
 
-export default function Pagination(props){
+export default function Pagination(props) {
+	const { data } = props;
+	const [currentItems, setCurrentItems] = useState([]);
+	const [pageCount, setPageCount] = useState(0);
+	const [itemOffset, setItemOffset] = useState(0);
+	const itemsPerPage = 3;
 
-    const { data } = props;
-    const [currentItems, setCurrentItems] = useState([]);
-    const [pageCount, setPageCount] = useState(0);
-    const [itemOffset, setItemOffset] = useState(0);
-    const itemsPerPage = 3;
+	useEffect(() => {
+		const endOffset = itemOffset + itemsPerPage;
+		setCurrentItems(data.slice(itemOffset, endOffset));
+		setPageCount(Math.ceil(data.length / itemsPerPage));
+	}, [itemOffset, itemsPerPage, data]);
 
-        useEffect(() => {
+	const handlePageClick = (event) => {
+		const newOffset = (event.selected * itemsPerPage) % data.length;
+		setItemOffset(newOffset);
+	};
 
-            const endOffset = itemOffset + itemsPerPage;
-            setCurrentItems (data.slice (itemOffset, endOffset));
-            setPageCount (Math.ceil(data.length / itemsPerPage));
-        }, [itemOffset, itemsPerPage, data]);
-    
-    const handlePageClick = (event) => {
-        const newOffset = (event.selected * itemsPerPage) % data.length;
-        setItemOffset(newOffset);
-    };
-
-    return (
-        <>
-        <ReactPaginate
-        breakLabel="..."
-            nextLabel="next >"
-            onPageChange={handlePageClick}
-            pageRangeDisplayed={3}
-            pageCount={pageCount}
-            previousLabel="< previous"
-            renderOnZeroPageCount={null}
-            />
-        </>
-    );
-
+	return (
+		<>
+			<ReactPaginate
+				breakLabel="..."
+				nextLabel="next >"
+				onPageChange={handlePageClick}
+				pageRangeDisplayed={3}
+				pageCount={pageCount}
+				previousLabel="< previous"
+				renderOnZeroPageCount={null}
+			/>
+		</>
+	);
 }
